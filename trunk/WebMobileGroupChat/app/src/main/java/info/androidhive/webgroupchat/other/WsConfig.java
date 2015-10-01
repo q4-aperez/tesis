3076dfa615afc91cb99adf92ea6d69f6a2533535
alexperez <alexperez@d0238042-a2bb-4db0-8620-ccfe8d0363b5>
2015-10-01 18:05:22 +0000

    import proy android

---
+++ trunk/WebMobileGroupChat/app/src/main/java/info/androidhive/webgroupchat/other/WsConfig.java
@@ -0,0 +1,8 @@
+package info.androidhive.webgroupchat.other;
+
+/**
+ * Created by alexperez on 27/09/2015.
+ */
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://192.168.0.7:8080/WebMobileGroupChatServer/chat?name=";
+}
