362c495867e2f4407b1153ba88da9cfd1fb70a12
alexperez <alexperez@d0238042-a2bb-4db0-8620-ccfe8d0363b5>
2016-04-13 20:41:16 +0000

    More classes

--- trunk/WebMobileGroupChat/MobileChatGroup/src/main/java/edu/aperez/webmobilegroupchat/MainActivity.java
+++ trunk/WebMobileGroupChat/MobileChatGroup/src/main/java/edu/aperez/webmobilegroupchat/MainActivity.java
@@ -18,20 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Processor receiver
-        AlarmManager service = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, ProcessorReceiver.class);
-        PendingIntent pending = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        // fetch every 30 seconds
-        // InexactRepeating allows Android to optimize the energy consumption
-        long secondsFromNow = System.currentTimeMillis() + 5 * 1000;
-        service.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                secondsFromNow, 30000, pending);
+//        //Processor receiver
+//        AlarmManager service = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        Intent intent = new Intent(this, ProcessorReceiver.class);
+//        PendingIntent pending = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//        // fetch every 30 seconds
+//        // InexactRepeating allows Android to optimize the energy consumption
+//        long secondsFromNow = System.currentTimeMillis() + 5 * 1000;
+//        service.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+//                secondsFromNow, 30000, pending);
+//
+//        //Battery Receiver
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+//        BatteryReceiver batteryReceiver = new BatteryReceiver();
+//        registerReceiver(batteryReceiver, intentFilter);
 
-        //Battery Receiver
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-        BatteryReceiver batteryReceiver = new BatteryReceiver();
-        registerReceiver(batteryReceiver, intentFilter);
+        startService(new Intent(this, DeviceInformationService.class));
     }
 }
